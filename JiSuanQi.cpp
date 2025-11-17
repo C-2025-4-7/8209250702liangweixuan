f18d5bf101377de0bcea0b07a660c4de36fecc48
Anontokyo-Mygo <1239613127@qq.com>
2025-11-17 20:12:02 +0800

    Add files via upload

---
+++ JiSuanQi.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>	
+using namespace std;
+int main()
+{
+	double a, b;
+	char jisuan;
+	cout << "请输入两个数字（数字 计算符 数字）" << endl;
+	cin >> a >>jisuan>> b;
+	switch (jisuan) {
+	case '+':
+		cout << a << "+" << b << "=" << a + b << endl;
+		break;
+	case'-':
+		cout << a << "-" << b << "=" << a - b << endl;
+		break;
+	case'*':
+		cout << a << "*" << b << "=" << a * b << endl;
+		break;
+	case'/':
+		if (b != 0)
+			cout << a << "/" << b << "=" << a / b << endl;
+		else
+			cout << "错误,除数不能为零" << endl;
+	case'%':
+		cout << a << "%" << b << "=" << (int)a % (int)b << endl;
+	}
+	return 0;
+}
