3af32fcc6fb8eaba38e0ad79882a517a6c826c7d
Anontokyo-Mygo <1239613127@qq.com>
2025-11-17 20:38:34 +0800

    Add files via upload

---
+++ Gong.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, m, n, temp;
+    cout << "请输入两个正整数：";
+    cin >> a >> b;
+    m = a;
+    n = b;
+    while (b != 0) {
+        temp = a % b;
+        a = b;
+        b = temp;
+    }
+    int lcm = (m * n) / a;
+    cout << "最大公约数：" << a << endl;
+    cout << "最小公倍数：" << lcm << endl;
+    return 0;
+}
