0840420e662120d9a3850a436e6084d165450190
Anontokyo-Mygo <1239613127@qq.com>
2025-11-17 19:11:43 +0800

    111
    222

---
+++ CeShi.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>  
+#include <iomanip>                                  
+using namespace std;
+int main()
+{
+	unsigned int testUnint = 65534;//0xfffe   
+	cout << "output in unsigned int  type:" << testUnint << endl;//<<oct;         
+	cout << "output in char type:" << static_cast<char>(testUnint) << endl;
+	cout << "output in short type:" << static_cast<short>(testUnint) << endl;  //为什么结果为-2？
+	cout << "output in int type:" << static_cast<int> (testUnint) << endl;
+	cout << "output in double type:" << static_cast<double>(testUnint) << endl;
+	cout << "output in double type:" << setprecision(4) << static_cast<double>(testUnint) << endl;
+	cout << "output in Hex unsigned int type:" << hex << testUnint << endl; //16进制输出	system("pause");                                    
+	cout << "output in Oct unsigned int type: " << oct << testUnint << endl;//⑧进制
+	double num1 = 3.3, num2 = -2.3;
+	cout << "int(3.3) = " << static_cast<int>(num1) << endl; 
+	cout << "int(-2.3) = " << static_cast<int>(num2) << endl; 
+	return 0;
+}
