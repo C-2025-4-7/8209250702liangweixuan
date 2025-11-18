9f17f6c9fbea6fe0c71379334516397e53155e81
Anontokyo-Mygo <1239613127@qq.com>
2025-11-18 19:17:42 +0800

    Add files via upload

---
+++ 平方根.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double a, x0, x1;
+    cout << "请输入a的值：";
+    cin >> a;
+
+    x0 = a / 2;
+    do {
+        x1 = 0.5 * (x0 + a / x0);
+        if (fabs(x1 - x0) < 1e-5) {
+            break;
+        }
+        x0 = x1;
+    } while (true);
+
+    cout << "√" << a << "=" << fixed << setprecision(6) << x1 << endl;
+    return 0;
+}
