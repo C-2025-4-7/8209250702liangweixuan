2960617a7bcd7c19edf2ee93ca8f356d1ed295ec
Anontokyo-Mygo <1239613127@qq.com>
2025-12-18 20:35:11 +0800

    Add files via upload

---
+++ func2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+bool is_prime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    if (num == 2) {
+        return true;
+    }
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int count = 0;
+    int num = 2;
+    while (count < 200) {
+        if (is_prime(num)) {
+            count++;
+            cout << setw(6) << num;
+            if (count % 10 == 0) {
+                cout << endl;
+            }
+        }
+        num++;
+    }
+    return 0;
+}
---
+++ func4.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include"mytriangle.h.h"
+using namespace std;
+int main() {
+	int a, b, c;
+	cout << "输入三条边长" << endl;
+	cin >> a >> b >> c;
+	if (is_valid(a, b, c)) {
+		cout << area(a, b, c) << endl;
+	}
+	else {
+		cout << "输入三条边长" << endl;
+		cin >> a >> b >> c;
+	}
+	return 0;
+}	
